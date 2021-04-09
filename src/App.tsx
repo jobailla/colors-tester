dd2beec08ee18de320a59fb58981836e2c440535
jobailla <jbaillais77@gmail.com>
2021-04-09 11:32:48 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,66 @@
-import React, { useState } from 'react';
-import { ChromePicker } from 'react-color';
+import React, { useState, useEffect } from 'react';
+import { ChromePicker, MaterialPicker } from 'react-color';
 import './styles/App.scss';
 
 function App() {
-  const [bgColor, setBgColor] = useState("#008080");
-  const [colorX, setColorX] = useState("#209CE8");
-  const [colorY, setColorY] = useState("#9040F7");
-  let divStyle = { backgroundColor: bgColor };
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 769);
 
-    const styleX = {
-        backgroundColor: colorX
-    }
-    const styleY = {
-        backgroundColor: colorY
-    }
+  const updateMedia = () => {
+    setIsMobile(window.innerWidth < 769);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateMedia);
+    return () => window.removeEventListener('resize', updateMedia);
+  });
+
+  const [bgColor, setBgColor] = useState("#FFFFFF");
+  const [colorX, setColorX] = useState("#191645");
+  const [colorY, setColorY] = useState("#43C6AC");
+
+  const [zIndexX, setZindeX] = useState(0) 
+  const [zIndexY, setZindeY] = useState(1) 
+
+  let styleBg = {
+    backgroundColor: bgColor
+  };
+
+  const styleX = {
+    backgroundColor: colorX,
+    zIndex: zIndexX
+  }
+  const styleY = {
+    backgroundColor: colorY,
+    zIndex: zIndexY 
+  }
+
+  const handleZindex = () => {
+    setZindeX(zIndexX === 0 ? 1 : 0)
+    setZindeY(zIndexY === 0 ? 1 : 0)
+  }
 
   return (
-    <div className="App" style={divStyle}>
+    <div className="App">
       <div className="App__colors">
-            <div className="colorGrid">
-            <div className="color1" style={styleX} />
-            <div className="color2" style={styleY} />
+        <div className="colorGrid" style={styleBg} >
+          <div className="color1" style={styleX} onClick={() => handleZindex()}/>
+          <div className="color2" style={styleY} onClick={() => handleZindex()}/>
         </div>
       </div>
       <div className="App__inputs">
-        <ChromePicker color={bgColor} onChangeComplete={newColor => setBgColor(newColor.hex)} />
-        <ChromePicker color={colorX} onChangeComplete={newColor => setColorX(newColor.hex)} />
-        <ChromePicker color={colorY} onChangeComplete={newColor => setColorY(newColor.hex)} />
+        {isMobile ?
+          <>
+            <MaterialPicker color={bgColor} onChangeComplete={newColor => setBgColor(newColor.hex)} />
+            <MaterialPicker color={colorX} onChangeComplete={newColor => setColorX(newColor.hex)} />
+            <MaterialPicker color={colorY} onChangeComplete={newColor => setColorY(newColor.hex)} />
+          </>
+          :
+          <>
+            <ChromePicker color={bgColor} onChangeComplete={newColor => setBgColor(newColor.hex)} />
+            <ChromePicker color={colorX} onChangeComplete={newColor => setColorX(newColor.hex)} />
+            <ChromePicker color={colorY} onChangeComplete={newColor => setColorY(newColor.hex)} />
+          </>
+        }
       </div>
     </div>
   );
